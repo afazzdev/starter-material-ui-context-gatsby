ada2c7562015f78794c4f3ca86941ccb4a8c0f16
afazzdev <afaizi1999@gmail.com>
2020-04-01 16:12:51 +0700

    fix title typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby starter Material-UI adn Context API`,
+    title: `Gatsby starter Material-UI and Context API`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@afazzdev`,
   },
